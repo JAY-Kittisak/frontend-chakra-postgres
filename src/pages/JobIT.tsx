093fc5c81a9f598c7a0083c98926586c30ec5430
JAY-Kittisak <71874439+JAY-Kittisak@users.noreply.github.com>
2021-09-15 17:31:03 +0700

    edit Job it.

--- src/pages/JobIT.tsx
+++ src/pages/JobIT.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Flex, Text, Divider, Button, Box } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { Flex, Text, Divider, Button, Box, Grid } from "@chakra-ui/react";
 import { Form, Formik, Field } from "formik";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 import InputField from "../components/InputField";
 
 interface Props { }
 
+const radioOptions = [
+    { name: "Altas", value: "altas" },
+    { name: "Hardware", value: "hardware" },
+    { name: "Software", value: "software" },
+    { name: "Network", value: "network" },
+    { name: "Email", value: "email" },
+    { name: "UPS", value: "ups" },
+    { name: "โทรศัพท์", value: "โทรศัพท์" },
+];
+
 const JobIT: React.FC<Props> = () => {
+    const [date, setDate] = useState(new Date());
+
+    const onChange = (date: Date) => {
+        setDate(date);
+    };
+
     return (
         <Flex flexDir="column">
             <Text
@@ -18,88 +36,89 @@ const JobIT: React.FC<Props> = () => {
             >
                 แจ้งงาน IT
             </Text>
-            <Divider mt={1} mb={5} orientation="horizontal" />
 
+            <Divider orientation="horizontal" />
 
-            <Flex
-                justifyContent="center"
-                bg="#eee"
-                h="100%"
-                p="10"
-                rounded="7px"
-            >
-                <Box
-                    w="600px"
-                    p="10"
-                    bg="#fff"
-                    rounded="7px"
-                    boxShadow="xl"
+            <Flex flexDir="column" align="center">
+                <Formik
+                    initialValues={{
+                        category: "",
+                        titled: "",
+                    }}
+                    onSubmit={async (values) => {
+                        console.log(values);
+                        console.log("desiredDate ==> ", date.toLocaleDateString());
+                    }}
                 >
-                    <Formik
-                        initialValues={{
-                            category: "",
-                            titled: "",
-                            desiredDate: "",
-                        }}
-                        onSubmit={async () => { }}
-                    >
-                        {({ isSubmitting }) => (
-                            <Form>
-                                <Text fontSize="xl">
-                                    ประเภท
-                                </Text>
-                                <Flex role="group" aria-labelledby="my-radio-group">
-                                    <label>
-                                        <Field
-                                            type="radio"
-                                            name="category"
-                                            value="Altas"
-                                        />
-                                        Altas
-                                    </label>
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Box
+                                w="600px"
+                                p="10"
+                                bg="#fff"
+                                rounded="7px"
+                                boxShadow="xl"
+                                mb="3"
+                            >
+                                <Text fontSize="xl">เลือกประเภท :</Text>
+                                <Grid
+                                    templateColumns={["repeat(3, 1fr)"]}
+                                    gap={6}
+                                    role="group"
+                                    aria-labelledby="my-radio-group"
+                                >
+                                    {radioOptions.map((radio, index) => (
+                                        <label key={index}>
+                                            <Field type="radio" name="category" value={radio.value} />
+                                            &nbsp; {radio.name}
+                                        </label>
+                                    ))}
+                                </Grid>
+                            </Box>
 
-                                    <label>
-                                        <Field
-                                            type="radio"
-                                            name="category"
-                                            value="hardware"
-                                        />
-                                        hardware
-                                    </label>
-
-                                    <label>
-                                        <Field
-                                            type="radio"
-                                            name="category"
-                                            value="software"
-                                        />
-                                        software
-                                    </label>
-                                </Flex>
-
+                            <Box
+                                w="600px"
+                                p="5"
+                                bg="#fff"
+                                rounded="7px"
+                                boxShadow="xl"
+                                mb="3"
+                            >
                                 <InputField
                                     textarea
                                     name="titled"
                                     placeholder="รายละเอียด..."
-                                    label="รายละเอียด"
+                                    label="รายละเอียด : "
                                 />
+                            </Box>
 
-                                <Button
-                                    w="100%"
-                                    mt="5"
-                                    colorScheme="green"
-                                    isLoading={isSubmitting}
-                                    type="submit"
-                                >
-                                    Save
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                </Box>
+                            <Box w="600px" p="5" bg="#fff" rounded="7px" boxShadow="xl" mb="3">
+                                <Text fontSize="xl">วันที่ต้องการ :</Text>
+                                <Flex>
+                                    <Flex bg="#006EDC" w="250px" p="10" align="center">
+                                        <Text fontSize="4xl" fontWeight="bold" color="white">
+                                            {date.toLocaleString()}
+                                        </Text>
+                                    </Flex>
+                                    <Calendar onChange={onChange} value={date} />
+                                </Flex>
+
+                            </Box>
+
+                            <Button
+                                w="100%"
+                                colorScheme="green"
+                                isLoading={isSubmitting}
+                                type="submit"
+                            >
+                                Save
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
             </Flex>
-        </Flex >
-    )
-}
+        </Flex>
+    );
+};
 
-export default JobIT
+export default JobIT;
